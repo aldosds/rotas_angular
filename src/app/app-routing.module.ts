79807c9bdb49befb82fef17844b1495f067010e8
Aldo Santos <aldoacesso@gmail.com>
2023-10-03 16:34:12 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portfolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TitleComponent, pathMatch: 'full'}, //full é pra quando quero a url exata
+  {path: 'portfolio', component: CardComponent, pathMatch: 'prefix'} //prefix é pra quando a url contenha algo no prefix
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
